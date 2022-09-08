a47391184f7f3c0f3f59ae7005c9023014028a13
Maqsood Pasha S N <97025769+maq090@users.noreply.github.com>
2022-09-08 14:32:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ prod_index=int(data[data['product']==selected_product].index.values[0])
             
 if st.button("Show Similar Products"):
     st.subheader("Similar Products for :" +data['product'].loc[prod_index])
-    similar_products=get_similar(prod_index): # num_results argument not passed as it is given a default value in function
+    similar_products=get_similar(prod_index) # num_results argument not passed as it is given a default value in function
     st.dataframe(similar_products)
 
 
