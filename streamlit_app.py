0d16b7f1d876c14a394ecdf1f6d1f7461ab53328
Maqsood Pasha S N <97025769+maq090@users.noreply.github.com>
2022-09-08 10:49:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.set_page_config(page_title="Big Basket Product Recommendation System",page_icon="logo.png",layout="wide")
+
+data=pd.read_csv('preprocessed_with_cluster_label.csv')
+st.title('Big Basket Product Recommendation System')
+st.write(data)
