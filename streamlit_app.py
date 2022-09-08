abab82725408b4fc5cf75594593998845e541c6d
Maqsood Pasha S N <97025769+maq090@users.noreply.github.com>
2022-09-08 10:59:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
+#references: streamlit.io.docs
+#https://github.com/vermaayush680/
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity  
+from sklearn.metrics import pairwise_distances
+from scipy.sparse import hstack
+import pickle
+from scipy.sparse import hstack
 
 st.set_page_config(page_title="Big Basket Product Recommendation System",page_icon="logo.png",layout="wide")
 
