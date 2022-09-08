3b96421c76300ec6ba4c9778c9118012594f9661
Maqsood Pasha S N <97025769+maq090@users.noreply.github.com>
2022-09-08 12:57:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
+import gzip
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity  
 from sklearn.metrics import pairwise_distances
 from scipy.sparse import hstack
-import pickle
 from scipy.sparse import hstack
-from pyunpack import Archive
+
 
 st.set_page_config(page_title="Big Basket Product Recommendation System",page_icon="logo.png",layout="wide")
 
@@ -27,8 +28,17 @@ data=load_data(file)
 
 #loading the glove_vector file which is in zipped form
 # used gzip form 
+#https://stackoverflow.com/questions/24906126/how-to-unpack-pkl-file
+@st.cache
+def loadglove(name):
+    """ function to laod glove vector file")"""
+    with gzip.open(name,'rb,) as f:
+        model=pickle.load(f)
+        return model
+file='glove_vectors.pkl.gz'                  
+model=loadglove(file)
 
-
+st.write(model['oil'])
 
 
 
