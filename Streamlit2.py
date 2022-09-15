67339c24693570704df8279f4afd044419130dba
Maqsood Pasha S N <97025769+maq090@users.noreply.github.com>
2022-09-15 18:23:09 +0530

    Update Streamlit2.py

--- Streamlit2.py
+++ Streamlit2.py
@@ -308,12 +308,12 @@ def get_similar_products(query,train_data=df,X_train=X_train,num_results=11):
 
 #image=Image.open('demo.png') # sample data point image 
 # file uploader to upload query datapoint, single point at a time
+image=Image.open('demo.png')
+st.image(image,caption="sample data point to upload")
 uploaded_file=st.file_uploader('Upload csv file of query product with all columns')
 #st.image(image, caption='Sample datapoint')
 
 num_results=11
-image=Image.open('demo.png')
-st.image(image,caption="sample data point")
 if uploaded_file is not None:
     query=pd.read_csv(uploaded_file)
     if missing_features(query):
